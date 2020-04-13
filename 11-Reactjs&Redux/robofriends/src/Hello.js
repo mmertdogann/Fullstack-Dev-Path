eb827443f84c93ae44479dd1596f2910e81d4507
mmertdogann <doganme@mef.edu.tr>
2020-04-13 21:41:58 +0300

    Getting started with React

---
+++ 11-Reactjs&Redux/robofriends/src/Hello.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+    render() {
+        return (
+        <div className='f1 tc'>
+            <h1>Hello World</h1>
+            <p>{this.props.greeting}</p>
+        </div>
+        );
+        
+    }
+}
+
+//Think the expression above like this (function)
+// const Hello = (props) => {
+//     return (
+//         <div className='f1 tc'>
+//             <h1>Hello World</h1>
+//             <p>{props.greeting}</p>
+//         </div>
+//     );
+// }
+
+
+export default Hello;
