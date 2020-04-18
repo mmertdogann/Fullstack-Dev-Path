b3643aa439f0e3794d8530ef09737ee12aa42b5e
mmertdogann <doganme@mef.edu.tr>
2020-04-18 15:57:23 +0300

    App updated

---
+++ 11-Reactjs&Redux/build/precache-manifest.2159d99d0e00118306f07c91c6f4241b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac4e63d94594fa8b2299e501588b3ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "16d0830f3c0c5286ff27",
+    "url": "/static/css/main.174e61fa.chunk.css"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d0830f3c0c5286ff27",
+    "url": "/static/js/main.ac96d469.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
